e7e9df67505997e8f119da46ff30924c9db0b036
ishwang <his8457@naver.com>
2022-08-13 23:13:07 +0900

    [백준] 정렬 [S5] 2751 수 정렬하기2 시간초과 -> 퀵소트로 다시 풀어볼 것.

---
+++ src/baekjoon/_05_sort/Main_2751.java
@@ -0,0 +1,57 @@
+package baekjoon._05_sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_2751 {
+	public static void main(String[] args) throws Exception {
+		// [백준] 정렬 [S5] 2751 수 정렬하기2#42
+		/*
+		 * 문제
+		 * N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+		 * 
+		 * 입력
+		 * 첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000,000)이 주어진다. 
+		 * 둘째 줄부터 N개의 줄에는 수가 주어진다. 
+		 * 이 수는 절댓값이 1,000,000보다 작거나 같은 정수이다. 
+		 * 수는 중복되지 않는다.
+		 * 출력
+		 * 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+
+		 * 예제 입력 1 
+		 * 5
+		 * 5
+		 * 4
+		 * 3
+		 * 2
+		 * 1
+		 * 예제 출력 1 
+		 * 1
+		 * 2
+		 * 3
+		 * 4
+		 * 5
+		 * */
+		
+		solve();
+	}
+
+	private static void solve() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int inputCount  = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[inputCount];
+		
+		for(int i = 0; i < inputCount; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i : arr) {
+			System.out.println(i);
+		}
+	}
+}
