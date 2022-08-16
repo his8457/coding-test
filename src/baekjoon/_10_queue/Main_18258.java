727c93d4e477abe4da4440d0f79cb17e09b5708c
ishwang <his8457@naver.com>
2022-08-16 21:36:36 +0900

    [백준] 18258 queue - 큐2 불필요한 import 제거

--- src/baekjoon/_10_queue/Main_18258.java
+++ src/baekjoon/_10_queue/Main_18258.java
@@ -3,8 +3,6 @@ package baekjoon._10_queue;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 
 public class Main_18258 {
 	public static void main(String[] args) throws Exception {
