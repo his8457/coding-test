a0b80bddfa429682630eb2fb7ee0bc7376364656
ishwang <his8457@naver.com>
2022-08-17 22:24:05 +0900

    [백준] 3009 기하1 - 네 번째 점 문제 풀기 완료

---
+++ src/baekjoon/_12_geometry/Main_3009.java
@@ -0,0 +1,73 @@
+package baekjoon._12_geometry;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main_3009 {
+	public static void main(String[] args) throws IOException {
+		// [백준] 3009 기하1 - 네 번째 점
+		/*
+		 * 문제
+		 * 세 점이 주어졌을 때, 축에 평행한 직사각형을 만들기 위해서 필요한 네 번째 점을 찾는 프로그램을 작성하시오.
+		 * 
+		 * 입력
+		 * 세 점의 좌표가 한 줄에 하나씩 주어진다. 
+		 * 좌표는 1보다 크거나 같고, 1000보다 작거나 같은 정수이다.
+		 * 
+		 * 출력
+		 * 직사각형의 네 번째 점의 좌표를 출력한다.
+		 * 
+		 * 예제 입력 1 
+		 * 5 5
+		 * 5 7
+		 * 7 5
+		 * 예제 출력 1 
+		 * 7 7
+		 * 
+		 * 예제 입력 2 
+		 * 30 20
+		 * 10 10
+		 * 10 20
+		 * 예제 출력 2 
+		 * 30 10
+		 * */
+		solve();
+	}
+
+	private static void solve() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] inputPoints = new String[3]; //세 점의 위치 
+		Map<Integer, Integer> xPoints = new HashMap<>();
+		Map<Integer, Integer> yPoints = new HashMap<>();
+		
+		for(int i = 0 ; i < inputPoints.length; i++) {
+			inputPoints[i] = br.readLine();
+			int x = Integer.parseInt(inputPoints[i].split(" ")[0]);
+			int y = Integer.parseInt(inputPoints[i].split(" ")[1]);
+			xPoints.put(x, xPoints.getOrDefault(x, 0)+1);
+			yPoints.put(y, yPoints.getOrDefault(y, 0)+1);
+		}
+		
+		int x = 0;
+		int y = 0;
+		for(Integer key : xPoints.keySet()) {
+			if(xPoints.get(key) == 1) {
+				x = key;
+				break;
+			}
+		}
+		
+		for(Integer key : yPoints.keySet()) {
+			if(yPoints.get(key) == 1) {
+				y = key;
+				break;
+			}
+		}
+		
+		System.out.println(x + " " + y);
+	}
+}
