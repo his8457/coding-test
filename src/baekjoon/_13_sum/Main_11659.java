ddb81ce971563823be0fb79c359de1c53ba26017
ishwang <his8457@naver.com>
2022-11-30 22:52:32 +0900

    [백준] 116589 구간합 구하기4 - 풀기 완료

--- src/baekjoon/_13_sum/Main_11659.java
+++ src/baekjoon/_13_sum/Main_11659.java
@@ -5,34 +5,33 @@ import java.io.InputStreamReader;
 
 public class Main_11659 {
 	public static void main(String[] args) throws Exception {
-		//[백준] 11659 누적합 -> 구간 합 구하기4
+		//[백준] 11659 구간합 구하기4
 		/*
 		 * 문제
-		 * 수 N개가 주어졌을 때, i번째 수부터 j번째 수까지 합을 구하는 프로그램을 작성하시오.
-		 * 
-		 * 입력
-		 * 첫째 줄에 수의 개수 N과 합을 구해야 하는 횟수 M이 주어진다. 
-		 * 둘째 줄에는 N개의 수가 주어진다. 
-		 * 수는 1,000보다 작거나 같은 자연수이다. 
-		 * 셋째 줄부터 M개의 줄에는 합을 구해야 하는 구간 i와 j가 주어진다.
-		 * 
-		 * 출력
-		 * 총 M개의 줄에 입력으로 주어진 i번째 수부터 j번째 수까지 합을 출력한다.
-		 * 
-		 * 제한
-		 * 1 ≤ N ≤ 100,000
-		 * 1 ≤ M ≤ 100,000
-		 * 1 ≤ i ≤ j ≤ N
-		 * 예제 입력 1 
-		 * 5 3
-		 * 5 4 3 2 1
-		 * 1 3
-		 * 2 4
-		 * 5 5
-		 * 예제 출력 1 
-		 * 12
-		 * 9
-		 * 1
+			수 N개가 주어졌을 때, i번째 수부터 j번째 수까지 합을 구하는 프로그램을 작성하시오.
+			
+			입력
+			첫째 줄에 수의 개수 N과 합을 구해야 하는 횟수 M이 주어진다. 
+			둘째 줄에는 N개의 수가 주어진다. 수는 1,000보다 작거나 같은 자연수이다. 
+			셋째 줄부터 M개의 줄에는 합을 구해야 하는 구간 i와 j가 주어진다.
+			
+			출력
+			총 M개의 줄에 입력으로 주어진 i번째 수부터 j번째 수까지 합을 출력한다.
+			
+			제한
+			1 ≤ N ≤ 100,000
+			1 ≤ M ≤ 100,000
+			1 ≤ i ≤ j ≤ N
+			예제 입력 1 
+			5 3
+			5 4 3 2 1
+			1 3
+			2 4
+			5 5
+			예제 출력 1 
+			12
+			9
+			1
 		 * */
 		
 		solve();
@@ -40,41 +39,34 @@ public class Main_11659 {
 
 	private static void solve() throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] inputStr = br.readLine().split(" ");
 		
-		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(inputStr[0]); //주어진 수의 개수
+		int M = Integer.parseInt(inputStr[1]); //합을 구해야 하는 횟수
 		
-		String firstInput = br.readLine(); 
-		String secondInput = br.readLine();
+		//문제 배열 생성
+		int[] input = new int[N+1];
+		int[] sumArr = new int[N+1];
+		inputStr = br.readLine().split(" ");
 		
-		int N = Integer.parseInt(firstInput.split(" ")[0]);
-		int M = Integer.parseInt(firstInput.split(" ")[1]);
-		int[] arr = new int[N];
-		
-		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(secondInput.split(" ")[i]);
+		for(int i = 1; i <= N; i++) {
+			input[i] = Integer.parseInt(inputStr[i-1]);
+			sumArr[i] = sumArr[i-1] + input[i];
 		}
 		
-		int start = 0;
-		int end = 0;
-		for(int i = 0; i < M; i++) {
-			String question = br.readLine();
-			start = Integer.parseInt(question.split(" ")[0]) - 1;
-			end = Integer.parseInt(question.split(" ")[1]) - 1;
-			
-			sb.append(sum(arr, start, end));
-			sb.append("\n");
-		}
-		
-		System.out.println(sb);
-	}
-
-	private static Object sum(int[] arr, int start, int end) {
+		//i에서 j까지 지문
 		int sum = 0;
 		
-		for(int i = start; i <= end; i++) {
-			sum += arr[i];
+		for(int k = 0; k < M; k++) {
+			sum = 0;
+			
+			String[] s = br.readLine().split(" ");
+			int i = Integer.parseInt(s[0]); //i
+			int j = Integer.parseInt(s[1]); //j
+			
+			sum = sumArr[j] - sumArr[i-1];
+
+			System.out.println(sum);
 		}
-		
-		return sum;
 	}
 }
